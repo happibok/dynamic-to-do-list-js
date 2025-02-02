551f350c7a360bc1de222d299551dbab858e0307
happibok <happiibok@gmail.com>
2025-02-01 23:52:06 -0500

    added the add task button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    const addButton = document.getElementById("add-task");
+    const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
