1cb5274a4eed780ac4e0f37fe276a7f42718d87e
happibok <happiibok@gmail.com>
2025-02-02 09:39:04 -0500

    added load task feature

--- script.js
+++ script.js
@@ -50,6 +50,11 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("tasks", JSON.stringify(storedTasks));
     }
 
+    function loadTasks() {
+        const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        storedTasks.forEach(taskText => addTask(taskText, false));
+    }
+
     addButton.addEventListener("click", function () {
         addTask(taskInput.value);
     });
